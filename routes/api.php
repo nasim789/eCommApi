7c2944c04f6dd997e4a84aa2c60349a81c7a34f4
Mohammad Ghani <mghani@Mohammads-MacBook-Pro.local>
2020-07-10 12:04:48 -0400

    Product and Review model with api routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Product routes
 Route::apiResource('/products', 'ProductController');
+// Reviews routes
 Route::group(['prefix' => 'products'], function(){
     Route::apiResource('/{product}/reviews', 'ReviewController');
 });
